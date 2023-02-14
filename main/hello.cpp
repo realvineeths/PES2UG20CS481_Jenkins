cba681a9cc135e842ed0f35aa07b578b896c81c2
realvineeths <vineeth14shivu@gmail.com>
2023-02-14 20:05:30 +0530

    after adding webhooks

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully re built and run using web hooks\n";
   cout << "~pes2ug20cs481";
   return 0;
 }
